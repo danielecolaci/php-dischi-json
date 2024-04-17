0abe30f670be23f8426a92284548fa2712fb60e3
danielecolaci <danielecolaci@outlook.com>
2024-04-17 17:32:39 +0200

    Import axios, vue, add script.js and style.css

--- index.php
+++ index.php
@@ -6,8 +6,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dischi JSON</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
+    <div id="app">
+        <h1>{{ message }}</h1>
+    </div>
     
+    <!-- Axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js" 
+    integrity="sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- Vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- script -->
+    <script src="./assets/js/script.js"></script>
 </body>
 </html>
