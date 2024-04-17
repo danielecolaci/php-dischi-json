53ef0df5903112c41782384af3da30b8db759a26
danielecolaci <danielecolaci@outlook.com>
2024-04-17 17:49:38 +0200

    add axios, get api.url and stamp data on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,21 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello'
+            url: 'api.php',
+            error: '',
+            dischi: []
         }
+    },
+    mounted() {
+        axios
+            .get(this.url)
+            .then((result) => {
+                console.log(result);
+                this.dischi = result.data;
+            }).catch((err) => {
+                console.log(err.message);
+                this.error = err.message;
+
+            })
     }
 }).mount('#app')
--- index.php
+++ index.php
@@ -10,9 +10,16 @@
 </head>
 <body>
     <div id="app">
-        <h1>{{ message }}</h1>
+        <ul v-for="disco in dischi">
+            <li>
+                <img :src="disco.poster" alt="">
+                <h3>{{ disco.title }}</h3>
+                <h5>{{ disco.author }}</h5>
+                <h4>{{ disco.year }}</h4>
+            </li>
+        </ul>
     </div>
-    
+
     <!-- Axios -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js" 
     integrity="sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==" 
